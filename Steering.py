0f0b7d79bd1f4a778d706a2921ec87e83efbbce8
SiddharthSrivatsan <sid.srivatsan@gmail.com>
2015-03-11 12:54:37 -0400

    Update Steering.py

--- Steering.py
+++ Steering.py
@@ -2,4 +2,14 @@ from random import randint
 import numpy as np
 import matplotlib.pyplot as plt
 
+class PVector(object):
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+  def returnX(self):
+    return self.x
+  def returnY(self):
+    return self.y
+
 class Vehicle(object):
+  def __init__(self, 
